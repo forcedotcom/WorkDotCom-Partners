808bb086e78c9b51f9b4e8cbf403253b24c67a0d
Jaswinder Rattanpal <jrattanpal@gmail.com>
2020-05-14 16:42:13 -0400

    SUbmit test data scripts from https://github.com/CovidBackToWork/workdotcom

---
+++ dx-utils/load_test_data.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Test Data script to load data into the default org
+# Default org will be your scratch org or whatever is set with "sfdx force:config:set defaultusername=OrgAlias"
+
+# This runs sfdx force:data:tree:import scripts load data into each object specified by file name
+# Example sfdx force:data:tree:import -f Employee__c.json will load data into Employee__c object
+
+
+echo "Load data into InternalOrganizationUnit, Employee and EmployeeHealthAssessment__c"
+{ # try
+    sfdx force:data:tree:import -p ./testdata/InternalOrganizationUnit-Employee-EmployeeHealthAssessment__c-plan.json
+    sfdx force:apex:execute -f ./dx-utils/apex-scripts/updateLocationVisitorAddressId.apex
+
+} || { # catch
+    echo "Unable to load data "
+}
+
+
+

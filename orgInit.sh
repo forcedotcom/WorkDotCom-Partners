ad5c75d602388913326217a4412e344cb8c93a5d
Jaswinder Rattanpal <jrattanpal@gmail.com>
2020-05-22 18:50:07 -0400

    Salesforce adds too many things in permission sets. so removing it for now

--- orgInit.sh
+++ orgInit.sh
@@ -1,17 +1,19 @@
 sfdx force:org:create -f config/project-scratch-def.json  -a WDCScratchOrg -s
 
+#SOurce code has a permission set with workplace license
+#This will auto assign WOrkplace license to the user
 sfdx force:source:push
 
 sfdx force:user:permset:assign -n Workplace_Command_Center_Standard_PermSet_Admin_Full_Access_Cloned
 
 #Command Center managed package
-sfdx force:package:install -p 04t5w000004iySq -k astrolovescodey -w 50
+sfdx force:package:install -p sfdx force:source -k astrolovescodey -w 50
 
 #Command Center un-managed package
 sfdx force:package:install -p 04t5w000005Is02 -k astrolovescodey -w 50
 
 #Workplace Permission Set Un-managed Package; pre-pacakge permissions sets as a starter step
-sfdx force:package:install -p 04t5w000004nA42
+#sfdx force:package:install -p 04t5w000004nA8F -w 10
 
 #Permission Sets
 #Workplace Admin
@@ -21,9 +23,12 @@ sfdx force:user:permset:assign -n b2w_GlobalOperations
 #Workplace Operations
 sfdx force:user:permset:assign -n b2w_Operations
 #All AddOn Permission Sets
-sfdx force:user:permset:assign -n b2w_GlobalOperationsAddOn
-sfdx force:user:permset:assign -n b2w_AdminAddOn
-sfdx force:user:permset:assign -n b2w_Workplace_Operations_Addon
+#sfdx force:user:permset:assign -n b2w_OperationsExecutiveAddOn
+#sfdx force:user:permset:assign -n b2w_Workplace_Operations_Addon
+#sfdx force:user:permset:assign -n b2w_Workplace_Command_Center_Access
+#sfdx force:user:permset:assign -n b2w_AdminAddOn
+#sfdx force:user:permset:assign -n b2w_GlobalOperationsExecutiveAddOn
+#sfdx force:user:permset:assign -n b2w_GlobalOperationsAddOn
 
 
 

71289fd6ca1ac4ba3a449d6d5560930f8289f484
Jaswinder Rattanpal <jrattanpal@gmail.com>
2020-05-22 17:23:07 -0400

    Assign AddOn permission sets for extra permissions

--- orgInit.sh
+++ orgInit.sh
@@ -10,6 +10,9 @@ sfdx force:package:install -p 04t5w000004iyOd -k astrolovescodey -w 50
 #Command Center un-managed package
 sfdx force:package:install -p 04t5w000005Is02 -k astrolovescodey -w 50
 
+#Workplace Permission Set Un-managed Package; pre-pacakge permissions sets as a starter step
+sfdx force:package:install -p 04t5w000004nA42
+
 #Permission Sets
 #Workplace Admin
 sfdx force:user:permset:assign -n b2w_Admin
@@ -17,6 +20,12 @@ sfdx force:user:permset:assign -n b2w_Admin
 sfdx force:user:permset:assign -n b2w_GlobalOperations
 #Workplace Operations
 sfdx force:user:permset:assign -n b2w_Operations
+#All AddOn Permission Sets
+sfdx force:user:permset:assign -n b2w_GlobalOperationsAddOn
+sfdx force:user:permset:assign -n b2w_AdminAddOn
+sfdx force:user:permset:assign -n b2w_Workplace_Operations_Addon
+
+
 
 #Location.Status__c is NOT included in this data set
 sfdx force:data:tree:import -p ./testdata/InternalOrganizationUnit-Employee-EmployeeCrisisAssessment-plan.json

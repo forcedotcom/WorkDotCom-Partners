2c325991d17432e1e806bd38affe64e31883e3b9
Jaswinder Rattanpal <jrattanpal@gmail.com>
2020-07-21 10:01:53 -0400

    Update Workplace command center version to v2.1

--- orgInit.sh
+++ orgInit.sh
@@ -2,10 +2,11 @@ sfdx force:org:create -f config/project-scratch-def.json  -a WDCScratchOrg -s -d
 
 
 #Command Center managed package
-sfdx force:package:install -p 04t5w000005dcR9 -w 50
+#v2.1
+sfdx force:package:install -p 04t5w000005dgQU -w 50
 
 #Command Center un-managed package
-sfdx force:package:install -p 04t5w000005au3o -w 50
+sfdx force:package:install -p 04t5w000005b0l1 -w 50
 
 
 #Install un-managed packages from Appiphony for their sample Building Management App

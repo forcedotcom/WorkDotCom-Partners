9edf362334bd8241400576d14971d25cdd3f851a
Jaswinder Rattanpal <jrattanpal@gmail.com>
2020-09-08 10:39:26 -0400

    Clean up the repo, remove extra permission sets because v3.0 doesn't need those. Create new permission sets via Apex in demo-setup.apex. Assign those new permission sets if those get created; if not the users have to manually assign those as the creation is async.

--- orgInit.sh
+++ orgInit.sh
@@ -12,6 +12,7 @@ sfdx force:package:install -p 04t5w000005mOu4 -w 50
 sfdx force:package:install -p 04t5w000005dhbI -w 50
 
 
+#Deactivating Appiphony app until error is fixed
 #Install un-managed packages from Appiphony for their sample Building Management App
 # https://github.com/appiphony/building-management-app
 sfdx force:package:install -p 04t5w000004Lpu3 -w 50
@@ -30,6 +31,9 @@ sfdx force:source:push
 #Perm sets have some fields from managed package so package needs to be installed first
 sfdx force:user:permset:assign -n Workplace_Command_Center_Standard_PermSet_Admin_Full_Access_Cloned
 
+#Custom Permission set to grant access to Location.Status__c that is in managed package
+sfdx force:user:permset:assign -n Workplace_Command_Center_Admin_Custom
+
 
 #Permission Sets
 #Workplace Admin
@@ -40,28 +44,32 @@ sfdx force:user:permset:assign -n b2w_OperationsExecutive
 sfdx force:user:permset:assign -n b2w_Operations
 
 #All AddOn Permission Sets
-sfdx force:user:permset:assign -n b2w_OperationsExecutiveAddOn
-sfdx force:user:permset:assign -n b2w_Workplace_Operations_Addon
-sfdx force:user:permset:assign -n b2w_Workplace_Command_Center_Access
-sfdx force:user:permset:assign -n b2w_AdminAddOn
-sfdx force:user:permset:assign -n b2w_GlobalOperationsExecutiveAddOn
-sfdx force:user:permset:assign -n b2w_GlobalOperationsAddOn
+#sfdx force:user:permset:assign -n b2w_OperationsExecutiveAddOn
+#sfdx force:user:permset:assign -n b2w_Workplace_Operations_Addon
+#sfdx force:user:permset:assign -n b2w_Workplace_Command_Center_Access
+#sfdx force:user:permset:assign -n b2w_AdminAddOn
+#sfdx force:user:permset:assign -n b2w_GlobalOperationsExecutiveAddOn
+#sfdx force:user:permset:assign -n b2w_GlobalOperationsAddOn
 
-#Custom Permission set to grant access to Location.Status__c that is in managed package
-sfdx force:user:permset:assign -n Workplace_Command_Center_Location_Admin
 
 #Appiphony app permission set
-sfdx force:user:permset:assign -n  Command_Center_Appiphony_App_Admin
+#sfdx force:user:permset:assign -n  Command_Center_Appiphony_App_Admin
 
-#Location.Status__c is NOT included in this data set
 sfdx force:data:tree:import -p ./testdata/InternalOrganizationUnit-Employee-EmployeeCrisisAssessment-plan.json
 
 sfdx force:apex:execute -f ./dx-utils/apex-scripts/updateLocationVisitorAddressId.apex
 
 #Post install file from Appiphony app
-sfdx force:apex:execute -f ./dx-utils/apex-scripts/convertData.apex
+#sfdx force:apex:execute -f ./dx-utils/apex-scripts/convertData.apex
 
 #Extra steps like resetting password for scartch org (if needed)
 sfdx force:apex:execute -f ./dx-utils/apex-scripts/demo-setup.apex
+
+#Permission set assignments - These were generated via demo-setup.apex file
+#These may not work sa above script seems async
+#In that case, assign these 3 manually after the org is created.
+sfdx force:user:permset:assign -n Workplace_Admin_Access_Group
+sfdx force:user:permset:assign -n Workplace_Command_Center_Access
+sfdx force:user:permset:assign -n Workplace_Admin_Objects
 
 sfdx force:org:open 

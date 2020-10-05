a4acf75357dc900ca7bb62fce39b4b6558ece3f9
Jaswinder Rattanpal <jrattanpal@gmail.com>
2020-10-05 19:24:47 -0400

    Add missing permissions set assignments in orgInit that caused 'Resource doesnt exist' error. Disable Appiphony app while waiting for fix. Disable Shift management as it takes time to install so make it on demand.

--- orgInit.sh
+++ orgInit.sh
@@ -15,8 +15,8 @@ sfdx force:package:install -p 04t5w000005dhbI -w 50
 #Deactivating Appiphony app until error is fixed
 #Install un-managed packages from Appiphony for their sample Building Management App
 # https://github.com/appiphony/building-management-app
-sfdx force:package:install -p 04t5w000004Lpu3 -w 50
-sfdx force:package:install -p 04t4S000000hXF3 -w 50
+#sfdx force:package:install -p 04t5w000004Lpu3 -w 50
+#sfdx force:package:install -p 04t4S000000hXF3 -w 50
 
 #Perm sets have some fields from managed package so package needs to be installed first
 
@@ -55,6 +55,9 @@ sfdx force:user:permset:assign -n b2w_Operations
 #Appiphony app permission set
 #sfdx force:user:permset:assign -n  Command_Center_Appiphony_App_Admin
 
+#Cleanup old data if needed
+#sfdx force:apex:execute -f ./dx-utils/apex-scripts/deleteTestData.apex
+
 sfdx force:data:tree:import -p ./testdata/InternalOrganizationUnit-Employee-EmployeeCrisisAssessment-plan.json
 
 sfdx force:apex:execute -f ./dx-utils/apex-scripts/updateLocationVisitorAddressId.apex
@@ -73,14 +76,15 @@ sfdx force:user:permset:assign -n Workplace_Command_Center_Access
 sfdx force:user:permset:assign -n Workplace_Admin_Objects
 
 #Shift Management Setup
+#DISABLED by default but run following 4 commands to install Shift Management and assign appropriate permission set
 #Install FSL package, get the latest version from https://fsl.secure.force.com/install
-sfdx force:package:install -p 04t3y000001yoTW -w 50
+#sfdx force:package:install -p 04t3y000001yoTW -w 50 -r
 #Install the Shift Management package, get the latest version from https://fsl.secure.force.com/work
-sfdx force:package:install -p 04t5w000005Hv9C -w 50
+#sfdx force:package:install -p 04t5w000005Hv9C -w 50
 
 #Assign Shift Management Workplace Manager Tabs permission set
-sfdx force:user:permset:assign -n WKFSL_Tabs_Workplace_Manager
+#sfdx force:user:permset:assign -n WKFSL_Tabs_Workplace_Manager
 #Assign Shift Management Shift Planner Tabs permission set
-sfdx force:user:permset:assign -n WKFSL_Tabs_Planner
+#sfdx force:user:permset:assign -n WKFSL_Tabs_Planner
 
 sfdx force:org:open 
